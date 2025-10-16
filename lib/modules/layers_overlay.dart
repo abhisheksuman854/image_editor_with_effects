60e67a01351f006ca9ed4ac4fcd88908edb80318
abhisheksuman854 <abhishek.suman854@gmail.com>
2025-10-16 15:11:13 +0530

    Update layers_overlay.dart

--- lib/modules/layers_overlay.dart
+++ lib/modules/layers_overlay.dart
@@ -4,6 +4,7 @@ import 'package:image_editor_with_effects/data/overlay_layer.dart';
 import 'package:image_editor_with_effects/image_editor_with_effects.dart';
 import 'package:image_editor_with_effects/modules/emoji_layer_overlay.dart';
 import 'package:image_editor_with_effects/modules/image_layer_overlay.dart';
+import 'package:image_editor_with_effects/modules/overlay_layer_overlay.dart';
 import 'package:image_editor_with_effects/modules/text_layer_overlay.dart';
 import 'package:reorderables/reorderables.dart';
 
@@ -24,6 +25,133 @@ class ManageLayersOverlay extends StatefulWidget {
 class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
   var scrollController = ScrollController();
 
+  // Helper method to build layer preview
+  Widget _buildLayerPreview(Layer layer) {
+    if (layer is LinkLayerData) {
+      return const Icon(
+        Icons.link,
+        size: 32,
+        color: Colors.white,
+      );
+    } else if (layer is TextLayerData) {
+      return Text(
+        'T',
+        style: const TextStyle(
+          fontSize: 32,
+          color: Colors.white,
+          fontWeight: FontWeight.w100,
+        ),
+      );
+    } else if (layer is EmojiLayerData) {
+      return Text(
+        layer.text,
+        style: const TextStyle(
+          fontSize: 32,
+          color: Colors.white,
+          fontWeight: FontWeight.w100,
+        ),
+      );
+    } else if (layer is ImageLayerData) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.memory(
+          layer.image.bytes,
+          fit: BoxFit.cover,
+          width: 40,
+          height: 40,
+        ),
+      );
+    } else if (layer is BackgroundLayerData) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.memory(
+          layer.image.bytes,
+          fit: BoxFit.cover,
+          width: 40,
+          height: 40,
+        ),
+      );
+    } else if (layer is OverlayLayerData) {
+      // Show overlay image preview
+      if (layer.overlayImage != null) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.memory(
+            layer.overlayImage!.bytes,
+            fit: BoxFit.cover,
+            width: 40,
+            height: 40,
+          ),
+        );
+      } else {
+        // Fallback icon if no image
+        return const Icon(
+          Icons.category,
+          size: 32,
+          color: Colors.white,
+        );
+      }
+    } else if (layer is BackgroundBlurLayerData) {
+      return const Icon(
+        Icons.blur_on,
+        size: 32,
+        color: Colors.white,
+      );
+    } else {
+      return const Text(
+        '',
+        style: TextStyle(color: Colors.white),
+      );
+    }
+  }
+
+  // Helper method to build layer title
+  Widget _buildLayerTitle(Layer layer) {
+    if (layer is LinkLayerData) {
+      return Text(
+        layer.text,
+        style: const TextStyle(color: Colors.white),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      );
+    } else if (layer is TextLayerData) {
+      return Text(
+        layer.text,
+        style: const TextStyle(color: Colors.white),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      );
+    } else if (layer is EmojiLayerData) {
+      return Text(
+        'Emoji: ${layer.text}',
+        style: const TextStyle(color: Colors.white),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      );
+    } else if (layer is OverlayLayerData) {
+      return const Text(
+        'Overlay Layer',
+        style: TextStyle(color: Colors.white),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      );
+    } else if (layer is BackgroundBlurLayerData) {
+      return const Text(
+        'Blur Layer',
+        style: TextStyle(color: Colors.white),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      );
+    } else {
+      return Text(
+        layer.runtimeType.toString(),
+        style: const TextStyle(color: Colors.white),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,24 +165,21 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
         ),
       ),
       child: ReorderableColumn(
-        // controller: scrollController,
-        // reverse: true,
-        // itemCount: layers.length,
         onReorder: (oldIndex, newIndex) {
-          var oi = layers.length - 1 - oldIndex,
-              ni = layers.length - 1 - newIndex;
+          var oi = widget.layers.length - 1 - oldIndex,
+              ni = widget.layers.length - 1 - newIndex;
 
           // skip main layer
           if (oi == 0 || ni == 0) {
             return;
           }
 
-          layers.insert(ni, layers.removeAt(oi));
+          widget.layers.insert(ni, widget.layers.removeAt(oi));
           widget.onUpdate();
           setState(() {});
         },
         draggedItemBuilder: (context, index) {
-          var layer = layers[layers.length - 1 - index];
+          var layer = widget.layers[widget.layers.length - 1 - index];
 
           return Container(
             width: double.infinity,
@@ -67,80 +192,20 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
                 SizedBox(
                   width: 64,
                   height: 64,
-                  child: Center(
-                    child: layer is TextLayerData || layer is EmojiLayerData
-                        ? Text(
-                            layer is TextLayerData
-                                ? 'T'
-                                : (layer as EmojiLayerData).text,
-                            style: const TextStyle(
-                              fontSize: 32,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w100,
-                            ),
-                          )
-                        : (layer is ImageLayerData
-                              ? ClipRRect(
-                                  borderRadius: BorderRadius.circular(8),
-                                  child: Image.memory(
-                                    layer.image.bytes,
-                                    fit: BoxFit.cover,
-                                    width: 40,
-                                    height: 40,
-                                  ),
-                                )
-                              : (layer is BackgroundLayerData
-                                    ? ClipRRect(
-                                        borderRadius: BorderRadius.circular(8),
-                                        child: Image.memory(
-                                          layer.image.bytes,
-                                          fit: BoxFit.cover,
-                                          width: 40,
-                                          height: 40,
-                                        ),
-                                      )
-                                    : (layer is OverlayLayerData
-                                          ? ClipRRect(
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                              child: Image.memory(
-                                                layer.overlayImage!.bytes,
-                                                fit: BoxFit.cover,
-                                                width: 40,
-                                                height: 40,
-                                              ),
-                                            )
-                                          : const Text(
-                                              '',
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                              ),
-                                            )))),
-                  ),
+                  child: Center(child: _buildLayerPreview(layer)),
                 ),
                 SizedBox(
                   width: MediaQuery.of(context).size.width - 92 - 64,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (layer is TextLayerData)
-                        Text(
-                          layer.text,
-                          style: const TextStyle(color: Colors.white),
-                        )
-                      else
-                        Text(
-                          layer.runtimeType.toString(),
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                    ],
+                    children: [_buildLayerTitle(layer)],
                   ),
                 ),
                 if (layer is! BackgroundLayerData)
                   IconButton(
                     onPressed: () {
-                      layers.remove(layer);
+                      widget.layers.remove(layer);
                       widget.onUpdate();
                       setState(() {});
                     },
@@ -151,12 +216,9 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
           );
         },
         children: [
-          for (var layer in layers.reversed)
-            // ReorderableWidget(
-            //   reorderable: true,
-            //   child:
+          for (var layer in widget.layers.reversed)
             GestureDetector(
-              key: Key('${layers.indexOf(layer)}:${layer.runtimeType}'),
+              key: Key('${widget.layers.indexOf(layer)}:${layer.runtimeType}'),
               onTap: () {
                 if (layer is BackgroundLayerData) return;
 
@@ -172,7 +234,7 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
                   builder: (context) {
                     if (layer is EmojiLayerData) {
                       return EmojiLayerOverlay(
-                        index: layers.indexOf(layer),
+                        index: widget.layers.indexOf(layer),
                         layer: layer,
                         onUpdate: () {
                           widget.onUpdate();
@@ -183,7 +245,7 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
 
                     if (layer is ImageLayerData) {
                       return ImageLayerOverlay(
-                        index: layers.indexOf(layer),
+                        index: widget.layers.indexOf(layer),
                         layerData: layer,
                         onUpdate: () {
                           widget.onUpdate();
@@ -194,8 +256,20 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
 
                     if (layer is TextLayerData) {
                       return TextLayerOverlay(
-                        index: layers.indexOf(layer),
+                        index: widget.layers.indexOf(layer),
                         layer: layer,
+                        onUpdate: () {
+                          widget.onUpdate();
+                          setState(() {});
+                        },
+                      );
+                    }
+
+                    // Add overlay layer editing if you have an overlay editor
+                    if (layer is OverlayLayerData) {
+                      return OverlayLayerOverlay(
+                        index: widget.layers.indexOf(layer),
+                        layerData: layer,
                         onUpdate: () {
                           widget.onUpdate();
                           setState(() {});
@@ -219,82 +293,20 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
                     SizedBox(
                       width: 64,
                       height: 64,
-                      child: Center(
-                        child: layer is LinkLayerData
-                            ? const Icon(
-                                Icons.link,
-                                size: 32,
-                                color: Colors.white,
-                              )
-                            : (layer is TextLayerData || layer is EmojiLayerData
-                                  ? Text(
-                                      layer is TextLayerData
-                                          ? 'T'
-                                          : (layer as EmojiLayerData).text,
-                                      style: const TextStyle(
-                                        fontSize: 32,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w100,
-                                      ),
-                                    )
-                                  : (layer is ImageLayerData
-                                        ? ClipRRect(
-                                            borderRadius: BorderRadius.circular(
-                                              8,
-                                            ),
-                                            child: Image.memory(
-                                              layer.image.bytes,
-                                              fit: BoxFit.cover,
-                                              width: 40,
-                                              height: 40,
-                                            ),
-                                          )
-                                        : (layer is BackgroundLayerData
-                                              ? ClipRRect(
-                                                  borderRadius:
-                                                      BorderRadius.circular(8),
-                                                  child: Image.memory(
-                                                    layer.image.bytes,
-                                                    fit: BoxFit.cover,
-                                                    width: 40,
-                                                    height: 40,
-                                                  ),
-                                                )
-                                              : const Text(
-                                                  '',
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                  ),
-                                                )))),
-                      ),
+                      child: Center(child: _buildLayerPreview(layer)),
                     ),
                     SizedBox(
                       width: MediaQuery.of(context).size.width - 92 - 64,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (layer is LinkLayerData)
-                            Text(
-                              layer.text,
-                              style: const TextStyle(color: Colors.white),
-                            )
-                          else if (layer is TextLayerData)
-                            Text(
-                              layer.text,
-                              style: const TextStyle(color: Colors.white),
-                            )
-                          else
-                            Text(
-                              layer.runtimeType.toString(),
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                        ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [_buildLayerTitle(layer)],
                       ),
                     ),
                     if (layer is! BackgroundLayerData)
                       IconButton(
                         onPressed: () {
-                          layers.remove(layer);
+                          widget.layers.remove(layer);
                           widget.onUpdate();
                           setState(() {});
                         },
@@ -307,7 +319,6 @@ class _ManageLayersOverlayState extends State<ManageLayersOverlay> {
                   ],
                 ),
               ),
-              // ),
             ),
         ],
       ),
